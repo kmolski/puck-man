8ba29d365db934bad1dd0ee320018e305042d0e0
kmolski <starfighter413@gmail.com>
2021-08-05 21:52:17 +0200

    Add SDL2 example application

--- game-state.lisp
+++ game-state.lisp
@@ -1,5 +1,7 @@
-(defconstant +time-to-start+ 10)
-(defconstant +default-lives+ 3)
+(require :sdl2)
+
+(defparameter *time-to-start* 10)
+(defparameter *default-lives* 3)
 
 (defclass game-state ()
   ((map :initarg :map
@@ -17,3 +19,30 @@
    (timer-start )
    (time-at-pause )
    (stage :initform 'init)))
+
+(defparameter *window-width* 800)
+(defparameter *window-height* 600)
+
+(defun sdl2-test ()
+  (sdl2:with-init (:everything)
+    (sdl2:with-window (win :title "SDL2 test"
+                           :flags '(:shown)
+                           :w *window-width*
+                           :h *window-height*)
+      (sdl2:with-renderer (renderer win :flags '(:accelerated))
+        (sdl2:with-event-loop (:method :poll)
+          (:keyup (:keysym keysym)
+                  (when (sdl2:scancode= (sdl2:scancode-value keysym)
+                                        :scancode-escape)
+                    (sdl2:push-event :quit)))
+          (:idle ()
+                 (multiple-value-bind (rects num)
+                     (apply #'sdl2:rects*
+                            (loop for x from 50 to (- *window-width* 50) by 150
+                                  append (loop for y from 50 to (- *window-height* 50) by 150
+                                               collect (sdl2:make-rect x y 100 100))))
+                   (sdl2:set-render-draw-color renderer 255 128 0 255)
+                   (sdl2:render-fill-rects renderer rects num))
+                 (sdl2:render-present renderer)
+                 (sdl2:delay 33))
+          (:quit () t))))))
