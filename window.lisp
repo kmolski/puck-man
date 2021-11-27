a182c02528cf497b463fd975fa8e67da66855763
kmolski <starfighter413@gmail.com>
2021-11-27 17:40:40 +0100

    Simplify window handling code

--- window.lisp
+++ window.lisp
@@ -5,26 +5,20 @@
 
 (defparameter *frame-delay* 32)
 
-(defun get-tile-edge (window-width window-height map)
-  "Calculate the map tile edge length (in pixels) based on
-  the window width+height and map dimensions."
-  (let* ((map-dimensions (array-dimensions (map-tiles map)))
-         (map-width  (first map-dimensions))
-         (map-height (second map-dimensions)))
-    (round (min (/ window-width map-width)
-                (/ window-height map-height)))))
+(defun get-tile-edge (window-width window-height map-width map-height)
+  "Calculate the map tile edge length (in pixels) based on the window width+height and map size."
+  (round (min (/ window-width map-width)
+              (/ window-height map-height))))
 
-(defun get-draw-start (window-width window-height map)
-  "Calculate the map position on the screen, based on
-  the window width+height and map dimensions."
-  (let* ((map-dimensions (array-dimensions (map-tiles map)))
-         (map-width  (first map-dimensions))
-         (map-height (second map-dimensions)))
-    (cons (floor (- (/ window-width  2) (/ (* map-width *tile-edge*)  2)))
-          (floor (- (/ window-height 2) (/ (* map-height *tile-edge*) 2))))))
+(defun get-draw-start (window-width window-height map-width map-height)
+  "Calculate the map position on the screen, based on the window width+height and map size."
+  (cons (floor (- (/ window-width  2) (/ (* map-width *tile-edge*)  2)))
+        (floor (- (/ window-height 2) (/ (* map-height *tile-edge*) 2)))))
 
 (defun recompute-draw-props (width height map)
-  "Recompute and update the map tile edge and position, based on
-  the given window width+height and game map dimensions"
-  (setf *tile-edge* (get-tile-edge width height map))
-  (setf *draw-start* (get-draw-start width height map)))
+  "Recompute and update the map tile edge and position."
+  (let* ((map-dimensions (array-dimensions (map-tiles map)))
+         (map-width  (first map-dimensions))
+         (map-height (second map-dimensions)))
+    (setf *tile-edge* (get-tile-edge width height map-width map-height))
+    (setf *draw-start* (get-draw-start width height map-width map-height))))
